35596bac152c53bc45f80dc00d484bb7d698bd95
ssprasad-23 <shynal_007@yahoo.com>
2024-09-20 03:00:50 -0700

    setup button and board

--- src/Components/TikTacToe/TicTacToe.jsx
+++ src/Components/TikTacToe/TicTacToe.jsx
@@ -5,8 +5,26 @@ import cross_icon from '../Assets/x.png'
 
 const TicTacToe = () => {
   return (
-    <div>
-      TicTacToe
+    <div className='container'>
+      <h1 className="title">Tic-Tac-Toe Game In <span>React</span></h1>
+      <div className="board">
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+      </div>  
+      <button className='reset'>Reset</button>
     </div>
   )
 }
